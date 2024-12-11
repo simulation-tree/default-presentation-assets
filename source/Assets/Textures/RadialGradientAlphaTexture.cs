918d8dbbf5780e8c4d3a9ea6d405987075f1b605
Phill <philldamaskin@gmail.com>
2024-12-11 12:24:10 -0500

    add radial gradient textures

---
+++ source/Assets/Textures/RadialGradientAlphaTexture.cs
@@ -0,0 +1,14 @@
+﻿using Data;
+using Unmanaged;
+
+namespace DefaultPresentationAssets
+{
+    public readonly struct RadialGradientAlphaTexture : IDataReference
+    {
+        static RadialGradientAlphaTexture()
+        {
+            EmbeddedAddress.Register<RadialGradientAlphaTexture>();
+        }
+        readonly FixedString IDataReference.Value => "Assets/Textures/RadialGradientAlpha.png";
+    }
+}
---
+++ source/Assets/Textures/RadialGradientBWTexture.cs
@@ -0,0 +1,15 @@
+﻿using Data;
+using Unmanaged;
+
+namespace DefaultPresentationAssets
+{
+    public readonly struct RadialGradientBWTexture : IDataReference
+    {
+        static RadialGradientBWTexture()
+        {
+            EmbeddedAddress.Register<RadialGradientBWTexture>();
+        }
+
+        readonly FixedString IDataReference.Value => "Assets/Textures/RadialGradientBW.png";
+    }
+}
