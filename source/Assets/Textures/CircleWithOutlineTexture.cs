aef40b7cbc02cf66042ad63266bad88e969419da
Phill <philldamaskin@gmail.com>
2024-12-17 13:30:47 -0500

    add circle with outline texture

---
+++ source/Assets/Textures/CircleWithOutlineTexture.cs
@@ -0,0 +1,15 @@
+﻿using Data;
+using Unmanaged;
+
+namespace DefaultPresentationAssets
+{
+    public readonly struct CircleWithOutlineTexture : IDataReference
+    {
+        static CircleWithOutlineTexture()
+        {
+            EmbeddedAddress.Register<CircleWithOutlineTexture>();
+        }
+
+        readonly FixedString IDataReference.Value => "Assets/Textures/CircleWithOutline.png";
+    }
+}
