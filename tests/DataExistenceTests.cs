f5a7837a0c19dc7f434fc223f202014ea520db7f
Phill <philldamaskin@gmail.com>
2025-02-19 15:55:07 -0500

    add tests

---
+++ tests/DataExistenceTests.cs
@@ -0,0 +1,40 @@
+﻿using Data;
+
+namespace DefaultPresentationAssets.Tests
+{
+    public class DataExistenceTests : DefaultPresentationAssetsTests
+    {
+        [Test]
+        public void VerifyTexturesExist()
+        {
+            Assert.That(EmbeddedResourceRegistry.Contains(EmbeddedResource.GetAddress<CheckerboardTexture>()), Is.True);
+            Assert.That(EmbeddedResourceRegistry.Contains(EmbeddedResource.GetAddress<CircleTexture>()), Is.True);
+            Assert.That(EmbeddedResourceRegistry.Contains(EmbeddedResource.GetAddress<TriangleTexture>()), Is.True);
+            Assert.That(EmbeddedResourceRegistry.Contains(EmbeddedResource.GetAddress<SquareTexture>()), Is.True);
+        }
+
+        [Test]
+        public void VerifyModelsExist()
+        {
+            Assert.That(EmbeddedResourceRegistry.Contains(EmbeddedResource.GetAddress<CubeModel>()), Is.True);
+            Assert.That(EmbeddedResourceRegistry.Contains(EmbeddedResource.GetAddress<SphereModel>()), Is.True);
+            Assert.That(EmbeddedResourceRegistry.Contains(EmbeddedResource.GetAddress<QuadModel>()), Is.True);
+        }
+
+        [Test]
+        public void VerifyFontsExist()
+        {
+            Assert.That(EmbeddedResourceRegistry.Contains(EmbeddedResource.GetAddress<CascadiaMonoFont>()), Is.True);
+            Assert.That(EmbeddedResourceRegistry.Contains(EmbeddedResource.GetAddress<RobotoFont>()), Is.True);
+        }
+
+        [Test]
+        public void VerifyShadersExist()
+        {
+            Assert.That(EmbeddedResourceRegistry.Contains(EmbeddedResource.GetAddress<TextFragmentShader>()), Is.True);
+            Assert.That(EmbeddedResourceRegistry.Contains(EmbeddedResource.GetAddress<TextVertexShader>()), Is.True);
+            Assert.That(EmbeddedResourceRegistry.Contains(EmbeddedResource.GetAddress<UnlitTexturedFragmentShader>()), Is.True);
+            Assert.That(EmbeddedResourceRegistry.Contains(EmbeddedResource.GetAddress<UnlitTexturedVertexShader>()), Is.True);
+        }
+    }
+}
---
+++ tests/DefaultPresentationAssetsTests.cs
@@ -0,0 +1,13 @@
+﻿using Data;
+using Unmanaged.Tests;
+
+namespace DefaultPresentationAssets.Tests
+{
+    public abstract class DefaultPresentationAssetsTests : UnmanagedTests
+    {
+        static DefaultPresentationAssetsTests()
+        {
+            EmbeddedResourceRegistry.Load<DefaultPresentationAssetsDataBank>();
+        }
+    }
+}
